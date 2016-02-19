6987863ec7f6dc55f983cc2c7696ceca370c5497
mdjc <mirnadjc@gmail.com>
2016-02-19 14:49:06 -0500

    Override toString

--- src/main/java/com/github/mdjc/hellomaven/App.java
+++ src/main/java/com/github/mdjc/hellomaven/App.java
@@ -2,6 +2,12 @@ package com.github.mdjc.hellomaven;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World!!");
+		System.out.println();
+	}
+
+	@Override
+	public String toString() {
+		return "APP";
 	}
 }
