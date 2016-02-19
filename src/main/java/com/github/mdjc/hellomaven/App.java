0178778709b089db41310fd73d078ab7ade99134
mdjc <mirnadjc@gmail.com>
2016-02-19 14:01:36 -0500

    Update hello message

--- src/main/java/com/github/mdjc/hellomaven/App.java
+++ src/main/java/com/github/mdjc/hellomaven/App.java
@@ -2,6 +2,6 @@ package com.github.mdjc.hellomaven;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello Maven");
+		System.out.println("Hello World!");
 	}
 }
