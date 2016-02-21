9d68bd75c880485e60d8e827a1594b24310d6e0a
mdjc <mirnadjc@gmail.com>
2016-02-20 20:47:53 -0500

    Add temp name

--- src/main/java/com/github/mdjc/hellomaven/Temp.java
+++ src/main/java/com/github/mdjc/hellomaven/Temp.java
@@ -1,5 +1,18 @@
 package com.github.mdjc.hellomaven;
 
 public class Temp {
+	private String name;
 
+	public Temp(String name) {
+		if (name == null) {
+			throw new IllegalArgumentException();
+		}
+
+		this.name = name.toUpperCase();
+	}
+
+	@Override
+	public String toString() {
+		return this.name;
+	}
 }
